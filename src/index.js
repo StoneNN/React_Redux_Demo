ada3c05018c8c02a54d3feb7047d68535cf6cc82
yanaheng <1677360167@qq.com>
2018-10-23 18:10:43 +0800

    create-react-app创建项目

--- src/index.js
+++ src/index.js
@@ -1,12 +1,27 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+import Proptypes from 'prop-types';
+import Header from './Header';
+import Content from './Content';
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: http://bit.ly/CRA-PWA
+
+class Index extends React.Component{
+    render(){
+        return(
+            <div>
+                <Header/>
+                <Content/>
+            </div>
+        )
+    }
+}
+
+ReactDOM.render(
+    <Index/>,
+    document.getElementById('root')
+)
+
 serviceWorker.unregister();
